eddd0a8bd2278dd64c2460f5f232828eac56f7a3
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2022-10-27 10:02:59 -0700

    clean interface

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { Box, Link } from "@mui/material";
+import RightsFields from "./RightsFields";
+
+const sx = {
+  root: {
+    minHeight: "98vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+  }
+};
+const App: FC = () => {
+  return (
+    <Box sx={sx.root}>
+      <RightsFields />
+      <Box>
+        <Link
+          href="https://github.com/tiavina-mika"
+          underline="none"
+          color="secondary.main"
+        >
+          Tiavina Michael RALAINIRINA
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default App;
